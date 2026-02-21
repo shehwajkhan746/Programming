ef1b89a00700403da1b621c41591072b239b93ea
SHAHBAZ ANSARI <shahbaz.earn@gmail.com>
2026-02-21 09:48:55 +0000

    String & Array Added

---
+++ Array/Array_int.c
@@ -0,0 +1,27 @@
+//Write a C program, to find both the largest and smallest number in a list of integers using an Array
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int i,min,max,size,a[20];
+printf("Enter size of the array:\n ");
+scanf("%d",&size);
+printf("Enter the array elements:\n");
+for(i=0; i<size; i++)
+{
+scanf("%d\n", &a[i]);
+}
+max=a[0];
+min=a[0];
+for(i=1;i<size;i++)
+{
+if(a[i]>max)
+max=a[i];
+if(a[i]<min)
+min=a[i];
+}
+printf("The maximum number in given array is: %d\n",max);
+printf("The minimum number in given arrary is: %d\n",min);
+return 0;
+}
