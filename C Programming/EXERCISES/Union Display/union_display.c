fa6ae262f76e75942673ff8c818787ef447d619c
SHAHBAZ ANSARI <shehwaj746@outlook.com>
2026-04-06 15:19:29 +0530

    Refactor union_display.c for improved readability

--- C Programming/EXERCISES/Union Display/union_display.c
+++ C Programming/EXERCISES/Union Display/union_display.c
@@ -4,29 +4,30 @@ union elements
 */
 
 #include <stdio.h>
-struct hai
-{
-char c;
-long l;
-char *p;
+struct hai {
+      char c;
+      long l;
+      char *p;
 };
-union bye
-{
-char c;
-long l;
-char *p;
+
+union bye {
+      char c;
+      long l;
+      char *p;
 };
-int main()
-{
-struct hai myhai;
-union bye mybye;
-myhai.c = 1;
-myhai.l = 2L;
-myhai.p = "This is myhai";
-mybye.c = 1;
-mybye.l = 2L;
-mybye.p = "This is mybye";
-printf("myhai: %d %ld %s\n", myhai.c, myhai.l, myhai.p);
-printf("mybye: %d %ld %s\n", mybye.c, mybye.l, mybye.p);
+
+int main() {
+  
+      struct hai myhai;
+      union bye mybye;
+      myhai.c = 1;
+      myhai.l = 2L;
+      myhai.p = "This is myhai";
+      mybye.c = 1;
+      mybye.l = 2L;
+      mybye.p = "This is mybye";
+  printf("myhai: %d %ld %s\n", myhai.c, myhai.l, myhai.p);
+  printf("mybye: %d %ld %s\n", mybye.c, mybye.l, mybye.p);
+  
 return 0;
 }
