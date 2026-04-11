7f525ed9db014c4b282cf446f038f0710ddf3dd2
SHAHBAZ ANSARI <shehwaj746@outlook.com>
2026-04-11 20:18:29 +0530

    Correct root calculation and add includes
    Fixed the calculation of root2 and added missing includes.

--- C Programming/COURSE/2. Basic Of C Programming/quadratic_equation_root.c
+++ C Programming/COURSE/2. Basic Of C Programming/quadratic_equation_root.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+main()
+{
+  float a, b, c, discriminant, root1, root2;
+  printf("Input the values of a, b and c\n");
+  scanf("%f %f %f", &a, &b, &c);
+
+  discriminant = b * b - 4 * a * c;
+  if (discriminant < 0){
+      printf("Roots are imaginary");
+  }
+  else {
+      root1 = (-b + sqrt(discriminant))/(2 * a);
+      root1 = (-b - sqrt(discriminant))/(2 * a);
+      printf("Root1 = %5.2f\n Root2 = %5.2f\n", root1, root2);
+  }
+return 0;
+)
